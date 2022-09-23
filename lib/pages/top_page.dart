343ed3553dee186fd6f3521a224616abb707d4a8
Yasushi Tamura <yasushitamura@YasushinoMacBook-Air.local>
2022-09-24 01:09:45 +0900

    トップページのUI作成

---
+++ lib/pages/top_page.dart
@@ -0,0 +1,20 @@
+//トップページのUIをここに記述する、stfulで雛形を作成できる
+import 'package:flutter/material.dart';
+
+class TopPage extends StatefulWidget {
+  const TopPage({Key? key}) : super(key: key);
+
+  @override
+  State<TopPage> createState() => _TopPageState();
+}
+
+class _TopPageState extends State<TopPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('ChatApp'),),
+      body: const Center(child: Text('Main Screen')),
+    );
+  }
+}
+
